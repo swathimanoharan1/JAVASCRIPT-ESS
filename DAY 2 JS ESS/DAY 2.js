1651dad3d742feafd14bfaef6867e451c0ad4899
swathimanoharan1 <86728223+swathimanoharan1@users.noreply.github.com>
2021-08-06 19:45:30 +0530

    DAY 2 JS Assignment by SWATHI MANOHARAN

---
+++ DAY 2 JS ESS/DAY 2.js
@@ -0,0 +1,16 @@
+// print numbers from 1 to 50 that are divisible by 5
+for (let i=1;i<=50;i++)
+{
+    if(i%5==0)
+    {
+        console.log(i);
+    }
+}
+
+// print five names in a array in reverse order 
+let arr=["Nairobi","Tokyo","Lisbon","Denver","Berlin"];
+{
+  reversed_arr=arr.reverse();
+  console.log(reversed_arr)
+
+}
