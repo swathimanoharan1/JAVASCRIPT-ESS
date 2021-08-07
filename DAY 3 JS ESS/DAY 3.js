7cda54e84b793e3cf8560f1aa915e2a56dc455c9
swathimanoharan1 <86728223+swathimanoharan1@users.noreply.github.com>
2021-08-07 23:56:49 +0530

    DAY 3 JS ESS by SWATHI MANOHARAN

---
+++ DAY 3 JS ESS/DAY 3.js
@@ -0,0 +1,10 @@
+function doSomething(){
+    let name=document.querySelector("#inp").value;
+   document.querySelector("#sec").innerHTML= `<h1>${name}</h1>`;
+}
+    
+    function doSomething(){
+        let city=document.querySelector("#cr").value;
+        document.querySelector("#sec").innerHTML= `<h1>${city}</h1>`;
+
+    }
